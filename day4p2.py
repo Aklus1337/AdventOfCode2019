f9bbd42dee912e9165831d6aa1dfffdda0f65d30
Aklus1337 <krzysztof.bienias.kb@gmail.com>
2020-10-18 00:23:39 +0200

    done

--- day4p2.py
+++ day4p2.py
@@ -1,36 +1,15 @@
 input = 0
 
-
 def dups(a):
     npairs = 0
-    apair = 0
     for x in range(0,5):
         if a[x] == a[x+1]:
             npairs += 1
-            apair.append(a[x+1])
-    if npairs > 0:
-        if len(set(apair)) == 1: # there are 2 types of pairs
-            return True
-    return False
-
-a=[1,2,3,4,4,4]
-
-print(dups(a))
-
-'''
-def dups(a):
-    npairs = 0
-    apair = 0
-    for x in range(0,5):
-        if a[x] == a[x+1]:
-            npairs += 1
-            apair.append(a[x+1])
-    if npairs > 0:
-        if len(set(apair)) == 1: # there are 2 types of pairs
-            return True
-    return False
-
-
+        else:
+            if npairs == 1:
+                return True
+            npairs = 0
+    return npairs == 1
 
 def find_dec(a):
     for d in range(0, 5):
@@ -38,14 +17,9 @@ def find_dec(a):
             return False
     return True
 
-
 for i in range(136818, 685979):
     re = [int(x) for x in str(i)]
     if dups(re) and find_dec(re):
         input += 1
 
-
 print(input)
-
-
-'''
